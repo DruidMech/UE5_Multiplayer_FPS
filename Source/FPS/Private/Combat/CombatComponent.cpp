c3ed23c8b146c13126c4c0dca4fb30fbda601541
Stephen Ulibarri <druidmech@gmail.com>
2026-06-19 15:34:18 -0600

    Combat Inputs

--- Source/FPS/Private/Combat/CombatComponent.cpp
+++ Source/FPS/Private/Combat/CombatComponent.cpp
@@ -20,3 +20,27 @@ void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 	
 }
 
+void UCombatComponent::Initiate_CycleWeapon()
+{
+}
+
+void UCombatComponent::Initiate_FireWeapon_Pressed()
+{
+}
+
+void UCombatComponent::Initiate_FireWeapon_Released()
+{
+}
+
+void UCombatComponent::Initiate_ReloadWeapon()
+{
+}
+
+void UCombatComponent::Initiate_Aim_Pressed()
+{
+}
+
+void UCombatComponent::Initiate_Aim_Released()
+{
+}
+
--- Source/FPS/Public/Combat/CombatComponent.h
+++ Source/FPS/Public/Combat/CombatComponent.h
@@ -16,6 +16,14 @@ public:
 	UCombatComponent();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
+	// Cycle to the next weapon in the inventory
+	void Initiate_CycleWeapon();
+	void Initiate_FireWeapon_Pressed();
+	void Initiate_FireWeapon_Released();
+	void Initiate_ReloadWeapon();
+	void Initiate_Aim_Pressed();
+	void Initiate_Aim_Released();
+	
 protected:
 	
 private:
