9cc8c541f367f8a759f16693839b8a08740e7c93
Stephen Ulibarri <druidmech@gmail.com>
2026-06-19 15:24:16 -0600

    Combat Component

---
+++ Source/FPS/Private/Combat/CombatComponent.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Druid Mechanics
+
+
+#include "Combat/CombatComponent.h"
+
+
+UCombatComponent::UCombatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	
+}
+
+
+
+void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
