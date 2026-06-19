91277408d1e3ee85cd879eed48a841c9153fe003
Stephen Ulibarri <druidmech@gmail.com>
2026-06-19 15:59:08 -0600

    Combat Callbacks

--- Source/FPS/Private/Combat/CombatComponent.cpp
+++ Source/FPS/Private/Combat/CombatComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Combat/CombatComponent.h"
 
+#include "Engine/Engine.h"
+
 
 UCombatComponent::UCombatComponent()
 {
@@ -22,25 +24,61 @@ void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 
 void UCombatComponent::Initiate_CycleWeapon()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_CycleWeapon"), 
+		false);
 }
 
 void UCombatComponent::Initiate_FireWeapon_Pressed()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_FireWeapon_Pressed"), 
+		false);
 }
 
 void UCombatComponent::Initiate_FireWeapon_Released()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_FireWeapon_Released"), 
+		false);
 }
 
 void UCombatComponent::Initiate_ReloadWeapon()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_ReloadWeapon"), 
+		false);
 }
 
 void UCombatComponent::Initiate_Aim_Pressed()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_Aim_Pressed"), 
+		false);
 }
 
 void UCombatComponent::Initiate_Aim_Released()
 {
+	GEngine->AddOnScreenDebugMessage(
+		-1, 
+		5.f, 
+		FColor::Cyan, 
+		TEXT("Initiate_Aim_Released"), 
+		false);
 }
 
