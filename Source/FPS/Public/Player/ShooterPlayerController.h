df3dcdbe60a4204215de4627f37f5bca2404efa7
Stephen Ulibarri <druidmech@gmail.com>
2026-07-01 15:30:27 -0600

    Alive

--- Source/FPS/Public/Player/ShooterPlayerController.h
+++ Source/FPS/Public/Player/ShooterPlayerController.h
@@ -24,7 +24,7 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
-	
+	virtual void OnPossess(APawn* InPawn) override;
 private:
 	
 	UPROPERTY(EditAnywhere, Category = "FPS|Input")
